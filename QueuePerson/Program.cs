49d228b6d0d91889feddb61ad8b90606a9e66c92
pavani <pavaniavni456@gmail.com>
2022-02-09 17:26:36 +0530

    added queueperson

---
+++ QueuePerson/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+public class QueuePerson
+{
+    public static void Main(string[] args)
+    {
+        Queue q = new Queue();
+
+        int total = 3;
+        for (int i = 0; i < total; i++)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            q.Enqueue(new Person()
+            {
+                Name = name,
+                Age = age
+            });
+        }
+
+        foreach (Person p in q)
+        {
+            Console.WriteLine(p.ToString());
+        }
+
+        Console.ReadLine();
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public override string ToString()
+        {
+            return Name + " - " + Age;
+        }
+    }
+}
